c1269fb5d3ac290c4db1ad58a99ef97ea925b6b4
Syed Muhammad Ali Aoun <80461232+AliAoun@users.noreply.github.com>
2024-01-03 15:13:14 +0500

    quiz

---
+++ blazor_api_task/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace blazor_api_task.Models
+{
+    public class Course
+    {
+        [Key]
+        public int CourseId { get; set; }
+        public string CourseName { get; set; }
+        public int Marks { get; set; }
+        public int StudentId { get; set; }
+
+      
+        public Student Student { get; set; }
+    }
+}
