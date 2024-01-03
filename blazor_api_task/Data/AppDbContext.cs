86f7ccb0c8a090aac75c8fa3691fa2fed93c750a
Syed Muhammad Ali Aoun <80461232+AliAoun@users.noreply.github.com>
2024-01-03 15:14:51 +0500

    quiz

--- blazor_api_task/Data/AppDbContext.cs
+++ blazor_api_task/Data/AppDbContext.cs
@@ -1,4 +1,4 @@
-﻿using blazor_api_task.Models;
+using blazor_api_task.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace blazor_api_task.Data
@@ -10,5 +10,24 @@ namespace blazor_api_task.Data
         }
 
         public DbSet<PatientData> Patients { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Student { get; set; }
+
+
+        var studentsWithLessThan50PercentMarks = AppDbContext.Student
+    .Where(s => s.Courses.Any(c => c.Marks < 50))
+    .ToList();
+
+        // Query for students with 50-85% marks
+        var studentsWith50To85PercentMarks = AppDbContext.Student
+            .Where(s => s.Courses.Any(c => c.Marks >= 50 && c.Marks <= 85))
+            .ToList();
+
+        // Query for students with 85%+ marks
+        var studentsWith85PlusPercentMarks = AppDbContext.Student
+            .Where(s => s.Courses.All(c => c.Marks >= 85))
+            .ToList();
+
+
     }
 }
